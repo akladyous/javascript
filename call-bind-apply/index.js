338519d9bcdbd7ab4be945232b6ad4dd0c716907
Boula Akladyous <akladyous@gmail.com>
2022-10-11 12:38:47 -0400

    call apply bind

---
+++ call-bind-apply/index.js
@@ -0,0 +1,8 @@
+const person = {
+    first_name: 'john',
+    last_name: 'doe',
+    getFullName: function () {
+        let fullName = this.first_name + ' ', this.last_name
+        return fullName;
+    }
+};
