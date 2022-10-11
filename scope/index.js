2de5287781361d726e5be6e77ab47f9d18ba4f28
Boula Akladyous <akladyous@gmail.com>
2022-10-11 12:04:49 -0400

    contenxt

--- scope/index.js
+++ scope/index.js
@@ -1,5 +1,21 @@
-let a = "not allowed to be globally scoped";
-var b = "global scope";
 
-console.log('a : ', a)
-console.log('b : ', b)
+function f1() {
+    function inner() {
+        console.log('this from inner function : ', this)
+    }
+    inner()
+}
+f1();
+const o1 = {
+    name: 'one',
+    log: function () {
+        this.name = 'two'
+        console.log('this from inside function in object 1:', this.name)
+        var setName = function (newName) {
+            this.name = newName;
+        };
+        setName('three')
+        console.log('this from inside function in object 2 :', this.name)
+    }
+}
+console.log(o1.log())
