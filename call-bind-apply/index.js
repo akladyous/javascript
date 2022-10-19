5c57e4b7cb287a4037b64d8c002b38225e901081
Boula Akladyous <akladyous@gmail.com>
2022-10-19 11:37:18 -0400

    added index.js to call-bind-apply

--- call-bind-apply/index.js
+++ call-bind-apply/index.js
@@ -2,7 +2,13 @@ const person = {
     first_name: 'john',
     last_name: 'doe',
     getFullName: function () {
-        let fullName = this.first_name + ' ', this.last_name
+        let fullName = this.first_name + ' ' + this.last_name;
         return fullName;
     }
 };
+
+const logName = function (lang1, lang2) {
+    console.log('logged : ', this.getFullName());
+};
+var logPersonName = logName.bind(person);
+logPersonName();
