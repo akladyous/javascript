078ea9332b37b2b286a4ca2e6e42345d5efe0300
Boula Akladyous <akladyous@gmail.com>
2022-11-12 15:55:41 -0500

    javascript

--- functions/function.js
+++ functions/function.js
@@ -1,6 +1,19 @@
 function greet() {
     console.log('ciao')
 };
-
 greet.language = 'english';
 console.log(greet.language)
+
+const people = {
+    firstName: 'john',
+    lastName: 'doe',
+    fullName: function () {
+        console.log("this is: ", this)
+        console.log([this.firstName, this.lastName].join(' '))
+        function setname() {
+            console.log('this inside setname function : ', this)
+        };
+        setname()
+    }
+}
+people.fullName()
