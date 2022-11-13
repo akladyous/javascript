6579ecc0d79b21ae354537ab18b57280ae9bca88
Boula Akladyous <akladyous@gmail.com>
2022-11-13 11:26:04 -0500

    js

---
+++ advanced/closures/clouser2.js
@@ -0,0 +1,20 @@
+function counter() {
+    var _count = 0
+    console.log('intial value of _count : ', _count)
+    function changeBy(value) { _count += value }
+    return {
+        increment(val = 1) {
+            changeBy(val)
+        },
+        decrement(val = -1) {
+            changeBy(val)
+        },
+        getValue() {
+            return _count
+        }
+    }
+}
+const counter1 = counter();
+counter1.increment();
+counter1.increment(3);
+console.log(counter1.getValue());
