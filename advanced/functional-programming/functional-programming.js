73b47f5d8848cbc32084ddf2923cd57402773d09
Boula Akladyous <akladyous@gmail.com>
2022-11-13 12:43:00 -0500

    js

--- advanced/functional-programming/functional-programming.js
+++ advanced/functional-programming/functional-programming.js
@@ -7,20 +7,30 @@ function mapForEach(arr, fn) {
     };
     return newArr
 }
-var arr1 = mapForEach(array1, (item) => {
+var arr1 = mapForEach(array1, item => {
     return item * 2
 })
-console.log(arr1)
+console.log("arr1 : ", arr1)
 //
 var checkPastLimit = function (limit, item) {
     return item > limit
 }
 var arr2 = mapForEach(array1, checkPastLimit.bind(this, 2))
-console.log(arr2)
+console.log("arr2 : ", arr2)
+//
 var checkPastLimitSimplified = function (limiter) {
     return function (limiter, item) {
         return item > limiter
     }.bind(this, limiter)
 }
 var arr3 = mapForEach(array1, checkPastLimitSimplified(2))
-console.log(arr3)
+console.log('arr3 : ', arr3)
+// -------------------------------------------------
+function multiply(a, b) {
+    return a * b
+}
+var presetArgument = function (value, fn) {
+    return fn.bind(this, value)
+}
+var result = presetArgument(2, multiply)
+console.log(result(2))
