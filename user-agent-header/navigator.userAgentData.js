75d24fd379d3d657f775bcf90e935cfb84d127bb
Boula Akladyous <akladyous@gmail.com>
2022-11-13 15:26:52 -0500

    js

--- user-agent-header/navigator.userAgentData.js
+++ user-agent-header/navigator.userAgentData.js
@@ -2,18 +2,18 @@
 
 (async () => {
     const highEntropyValues = await window.navigator.userAgentData.getHighEntropyValues(
-      ["platformVersion", "architecture", "bitness", "model", "uaFullVersion"]);
+        ["platformVersion", "architecture", "bitness", "model", "uaFullVersion"]);
     const platform = highEntropyValues.platform;               // "macOS"
     const platformVersion = highEntropyValues.platformVersion; //"10_15_4"
     const architecture = highEntropyValues.architecture;       // "x86"
     const bitness = highEntropyValues.bitness;                 // "64"
     const model = highEntropyValues.model;                     // ""
     const uaFullVersion = highEntropyValues.uaFullVersion;     // "84.0.4113.0"
-    
-      console.log("platform : ", platform)
-      console.log("platformVersion : ", platformVersion)
-      console.log("architecture : ", architecture)
-      console.log("bitness : ", bitness)
-      console.log("model : ", model)
-      console.log("uaFullVersion : ", uaFullVersion)
-  })();
+
+    console.log("platform : ", platform)
+    console.log("platformVersion : ", platformVersion)
+    console.log("architecture : ", architecture)
+    console.log("bitness : ", bitness)
+    console.log("model : ", model)
+    console.log("uaFullVersion : ", uaFullVersion)
+})();
