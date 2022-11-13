1594094f788d63c3f18d7782abedfad1a2abd792
Boula Akladyous <akladyous@gmail.com>
2022-11-13 10:55:40 -0500

    js

--- call-bind-apply/index.js
+++ call-bind-apply/index.js
@@ -6,19 +6,18 @@ const person = {
         return fullName;
     }
 };
-const person2 = {
-    firstName: 'Alex',
-    lastName: 'Doe'
-}
-
 const logName = function (lang1, lang2) {
     console.log('logged : ', this.getFullName());
     console.log('arguments : ', lang1, lang2)
 };
 var logPersonName = logName.bind(person);
 logPersonName('en', 'it');
-
+// function borrowing
+const person2 = {
+    firstName: 'Alex',
+    lastName: 'Doe'
+}
 logName.call(person, 'it', 'en')
 logName.apply(person, ['xx', 'yy'])
-
 console.log(person.getFullName.call(person2));
+// function curring
