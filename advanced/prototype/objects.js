ccba2c4669c22511834e227f454f84f4e5fc3dfe
Boula Akladyous <akladyous@gmail.com>
2023-04-10 17:02:32 -0400

    Untracked files on javascript

--- advanced/prototype/objects.js
+++ advanced/prototype/objects.js
@@ -9,6 +9,9 @@ const profile = {
     console.log(this.name);
   },
 };
+if (Object.isExtensible(person)) {
+  Object.setPrototypeOf(profile, person);
+}
 Object.setPrototypeOf(profile, person);
 console.log('person                                    : ', person);
 console.log('profile                                   : ', profile);
@@ -17,13 +20,24 @@ console.log('profile.firstName : ', profile.name);
 console.log('getPrototypeOf(profile)            : ', Object.getPrototypeOf(profile));
 console.log('profile.__proto__)                        : ', profile.__proto__);
 console.log('person.isPrototypeOf(profile)             : ', person.isPrototypeOf(profile));
-console.log('Object.getOwnPropertyDescriptors(profile) : ', Object.getOwnPropertyDescriptors(profile));
+console.log(
+  'Object.getOwnPropertyDescriptors(profile) : ',
+  Object.getOwnPropertyDescriptors(profile)
+);
 console.log('getOwnPropertyNames(profile)       : ', Object.getOwnPropertyNames(profile));
 console.log('-----------------------------------');
 const o = Object.create(null);
-console.log('const o = Object.create(null)             : ', o, ' create an object null as prototype');
+console.log(
+  'const o = Object.create(null)             : ',
+  o,
+  ' create an object null as prototype'
+);
 console.log('-----------------------------------');
 const obj1 = {};
 console.log('const obj1 = {}                           : ', o, ' create an object');
 const obj2 = Object.create(Object.prototype);
-console.log('obj2 = Object.create(Object.prototype)    : ', o, ' is equivalent to ->  const obj1 = {}');
+console.log(
+  'obj2 = Object.create(Object.prototype)    : ',
+  o,
+  ' is equivalent to ->  const obj1 = {}'
+);
