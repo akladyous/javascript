333cd054a64998ef54acd90c63bf50de7a0fc7f3
Boula Akladyous <akladyous@gmail.com>
2023-04-12 17:42:42 -0400

    Untracked files on javascript

--- advanced/prototype/context.js
+++ advanced/prototype/context.js
@@ -15,9 +15,9 @@ function foo() {
   console.log('declared variable "x" : ', x);
   function bar() {
     console.group('inner function');
+    console.log('declared variable "y" : ', y);
     console.log('this : ', this);
     var y = 2;
-    console.log('declared variable "y" : ', y);
     console.log('access varibale "x" from outer function x : ', x);
     console.log('value of y : ', y);
   }
@@ -26,4 +26,18 @@ function foo() {
   // console.log('inside outer function - : y ', y);
 }
 foo();
-//
+
+function a() {
+  console.log('a');
+  b();
+  console.log('a again');
+}
+function b() {
+  console.log('b');
+  c();
+  console.log('b again');
+}
+function c() {
+  console.log('c');
+}
+a();
