eff94bccf74e3dd4e02d2cb075efb379ac904cef
Boula Akladyous <akladyous@gmail.com>
2023-04-13 00:56:20 -0400

    Untracked files on javascript

--- advanced/prototype/test.js
+++ advanced/prototype/test.js
@@ -1,13 +1,12 @@
-
 function product(name, price) {
-    this.name = name;
-    this.price = price;
+  this.name = name;
+  this.price = price;
 }
 
 function food(name, price) {
-    product.call(this, name, price);
-    this.category = 'food';
-    console.log(this)
+  product.call(this, name, price);
+  this.category = 'food';
+  console.log(this);
 }
 
-console.log(new food('cheese', 5).name)
+console.log(new food('cheese', 5).name);
