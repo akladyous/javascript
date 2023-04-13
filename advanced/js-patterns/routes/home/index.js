9bc1711204c510195692242e53a8d78ca9e11dca
Boula Akladyous <akladyous@gmail.com>
2023-04-13 01:01:54 -0400

    Untracked files on javascript

--- advanced/js-patterns/routes/home/index.js
+++ advanced/js-patterns/routes/home/index.js
@@ -2,6 +2,5 @@ import express from 'express';
 export const home = express.Router();
 
 home.get('/', (req, res) => {
-            res.status(200).json({ message: "home page" })
+  res.status(200).json({ message: 'home page' });
 });
-
--- advanced/js-patterns/routes/users/index.js
+++ advanced/js-patterns/routes/users/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 export const users = express.Router();
 
 const usersHome = (req, res) => {
-    res.status(200).json({ data: 'aaaaa' })
-}
+  res.status(200).json({ data: 'aaaaa' });
+};
 
-users.get('/users', usersHome)
-users.all("/users/*", (req, res, next) => {
-    res.status(404).json({message: 'path not found'})
-})
+users.get('/users', usersHome);
+users.all('/users/*', (req, res, next) => {
+  res.status(404).json({ message: 'path not found' });
+});
