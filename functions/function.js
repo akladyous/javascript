7535eefa86686b6a69eb34068c62b90ec13f0b64
Boula Akladyous <akladyous@gmail.com>
2023-04-13 01:05:35 -0400

    Untracked files on javascript

--- functions/function.js
+++ functions/function.js
@@ -1,19 +1,19 @@
 function greet() {
-    console.log('ciao')
-};
+  console.log('ciao');
+}
 greet.language = 'english';
-console.log(greet.language)
+console.log(greet.language);
 
 const people = {
-    firstName: 'john',
-    lastName: 'doe',
-    fullName: function () {
-        console.log("this is: ", this)
-        console.log([this.firstName, this.lastName].join(' '))
-        function setname() {
-            console.log('this inside setname function : ', this)
-        };
-        setname()
+  firstName: 'john',
+  lastName: 'doe',
+  fullName: function () {
+    console.log('this is: ', this);
+    console.log([this.firstName, this.lastName].join(' '));
+    function setname() {
+      console.log('this inside setname function : ', this);
     }
-}
-people.fullName()
+    setname();
+  },
+};
+people.fullName();
