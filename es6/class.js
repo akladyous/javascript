1a2b99d3c88204cb02478029c52abfb1617a2ab1
Boula Akladyous <akladyous@gmail.com>
2023-04-14 16:41:06 -0400

    Untracked files on javascript

--- es6/class.js
+++ es6/class.js
@@ -3,8 +3,30 @@ class User {
     this.userName = userName;
     this.password = password;
   }
-
+  get getPassword() {
+    return this.password;
+  }
+  set setPassword(newPassword) {
+    this.password = newPassword;
+  }
   resetPassword(newPassword) {
     this.password = newPassword;
   }
+
+  static minimumPasswordLength = 6;
+
+  #description = 'User Class';
+  get description() {
+    return this.#description;
+  }
 }
+
+class Profile extends User {
+  constructor(userName, password, age) {
+    super(userName, password);
+    this.age = age;
+  }
+}
+
+var alex = new User('alex', '1234');
+console.log(alex);
