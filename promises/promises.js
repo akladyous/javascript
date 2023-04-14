060963c5ff4ce084f3288ee019fe838624e17e49
Boula Akladyous <akladyous@gmail.com>
2023-04-14 17:00:48 -0400

    Untracked files on javascript

--- promises/promises.js
+++ promises/promises.js
@@ -1,4 +1,4 @@
-const axios = require("axios").default;
+const axios = require('axios').default;
 
 //https://jsonplaceholder.typicode.com/todos
 
@@ -22,18 +22,18 @@ const axios = require("axios").default;
 //     })
 // ----------------------------------------------------------------
 const getUserEmails = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    const emailsArray = data.map((user) => {
-        return user.email;
-    });
+  const emailsArray = data.map((user) => {
+    return user.email;
+  });
 
-    postToWebPage(emailsArray);
+  postToWebPage(emailsArray);
 };
 
 const postToWebPage = (data) => {
-    console.log(data);
+  console.log(data);
 };
 
 // getUserEmails("https://jsonplaceholder.typicode.com/users");
@@ -41,30 +41,25 @@ const postToWebPage = (data) => {
 const controller = new AbortController();
 const signal = controller.signal;
 const getUsers = async (url) => {
-    try {
-        const response = await fetch(
-            "https://jsonplaceholder.typicode.com/users",
-            {
-                method: "GET",
-                signal: signal,
-            }
-        );
-        if (response.status !== 200) {
-            throw new Error(response.message);
-        }
-        const data = await response.json();
-        return data;
-    } catch (e) {
-        console.log(e.message);
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET',
+      signal: signal,
+    });
+    if (response.status !== 200) {
+      throw new Error(response.message);
     }
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e.message);
+  }
 };
-console.log("start");
+console.log('start');
 let start = performance.now();
-axios
-    .get("http://localhost:4000/sleep/5000")
-    .then(response => {
-        console.log(response.data)
-    })
+axios.get('http://localhost:4000/sleep/5000').then((response) => {
+  console.log(response.data);
+});
 let end = performance.now();
-console.log("end");
+console.log('end');
 console.log(Number.parseFloat(end - start).toFixed(2));
