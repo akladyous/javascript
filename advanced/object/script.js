8237d5fb6ff6d206f5d4c926846746e9087e350a
Boula Akladyous <akladyous@gmail.com>
2023-04-14 14:28:51 -0400

    Untracked files on javascript

--- advanced/object/script.js
+++
@@ -1,8 +0,0 @@
-console.log('------------------- objects -------------------');
-var obj = new Object();
-console.log(obj.constructor == Object);
-console.log(obj);
-
-const arr = [];
-console.log(Object.hasOwn(arr, 'constructor'));
-console.log(obj.hasOwnProperty('constructor'));
