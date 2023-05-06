29298a03eaad455753aa15b7f8a82e8d8b1d3359
Boula Akladyous <akladyous@gmail.com>
2023-05-06 11:23:57 -0400

    Untracked files on javascript

--- src/Jest/sumTwoNumbers.js
+++ src/Jest/sumTwoNumbers.js
@@ -1,5 +1,8 @@
-const sumTwoNumbers = (a, b) => {
+export const sumTwoNumbers = (a, b) => {
+  if (parseInt(a) < 0 || parseInt(b) < 0) {
+    throw new Error('Error: only positive integers');
+  }
   return a + b;
 };
 
-module.exports = sumTwoNumbers;
+// module.exports = sumTwoNumbers;
--- src/Jest/sumTwoNumbers.test.js
+++ src/Jest/sumTwoNumbers.test.js
@@ -1,5 +1,20 @@
-const sumTwoNumbers = require('./sumTwoNumbers');
+// const sumTwoNumbers = require('./sumTwoNumbers');
+import { describe, expect, it } from '@jest/globals';
+import { sumTwoNumbers } from './sumTwoNumbers';
 
-test('return the sum of two numbers', () => {
-  expect(sumTwoNumbers(1, 2).toBe(3));
+describe('sumTwoNumbers function', () => {
+  test('test 1+1 equals 2', () => {
+    expect(sumTwoNumbers(1, 1)).toBe(2);
+  });
+
+  test('allow only positive integers', () => {
+    expect(() => {
+      sumTwoNumbers(-1, 1);
+    }).toThrow('Error: only positive integers');
+  });
+});
+
+test('is a number', () => {
+  const num1 = 1;
+  expect(Number.isInteger(num1)).toBeTruthy();
 });
