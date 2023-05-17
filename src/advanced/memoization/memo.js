66d844bca9522642c9fe9b4995b64aa927e2a20d
Boula Akladyous <akladyous@gmail.com>
2023-05-17 10:03:41 -0400

    Untracked files on memoization

---
+++ src/advanced/memoization/memo.js
@@ -0,0 +1,23 @@
+const memo = (fn) => {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn(...args);
+    cache[args] = result;
+    return result;
+  };
+};
+
+const computionalMathFunc = (num) => {
+  for (let i = 0; i <= num; i++) {
+    if (i === num) {
+      return i;
+    }
+  }
+};
+
+console.time('2000_000_000 times');
+console.log(computionalMathFunc(2000000000));
+console.timeEnd('2000_000_000 times');
