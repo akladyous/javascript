e2a70992b05a7b35016bd2c3070d4ac69d7ef015
Boula Akladyous <akladyous@gmail.com>
2023-05-17 11:27:01 -0400

    Untracked files on javascript

--- src/advanced/react-useState-clone/useState.js
+++ src/advanced/react-useState-clone/useState.js
@@ -1,16 +1,35 @@
+// not done yet... still working on it!!
+
 function useState(initialValue) {
-  let _val = initialValue;
-  const state = () => {
-    return _val;
+  debugger;
+  const internalState = {
+    value: initialValue,
+    get state() {
+      return this.value;
+    },
+    set state(newValue) {
+      debugger;
+      this.value = newValue;
+    },
   };
 
+  const getValue = () => internalState.state;
   const setState = (newValue) => {
-    _val = newValue;
+    internalState.state = newValue;
   };
-  return [state, setState];
+  // let _val = initialValue;
+  // const state_ = () => _val;
+
+  // const setState = (newValue) => {
+  //   _val = newValue;
+  // };
+
+  return [getValue, setState];
 }
 
 const [count, setCount] = useState(1);
 console.log(count);
 setCount(2);
+console.log(count);
+setCount(3);
 console.log(count);
