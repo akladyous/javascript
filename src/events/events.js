2c3b100d51d5aa401b2c02e33d187ab2892badc2
Boula Akladyous <akladyous@gmail.com>
2023-05-18 17:57:51 -0400

    Untracked files on javascript

--- src/events/events.js
+++ src/events/events.js
@@ -1,7 +1,9 @@
-const main = document.querySelector('.main');
+const mainSection = document.querySelector('.main');
 
 const myEvent = new Event('myCustomEvent');
 
-main.addEventListener('myCustomEvent', (e) => {
+mainSection.addEventListener('myCustomEvent', (e) => {
   console.log(e);
 });
+
+document.dispatchEvent(myEvent);
