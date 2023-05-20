442fbdd7547d976a4d36e0cc7a2816c23adc9dc0
Boula Akladyous <akladyous@gmail.com>
2023-05-20 16:24:48 -0400

    Untracked files on javascript

--- src/frontend/scrolling/scroll.js
+++ src/frontend/scrolling/scroll.js
@@ -37,27 +37,33 @@ onload = () => {
     scroller.scrollHeight - Math.round(scroller.scrollTop) ===
     scroller.clientHeight;
 };
-// function checkReading() {
-//   if (checkReading.read) {
-//     return;
-//   }
-//   checkReading.read =
-//     this.scrollHeight - Math.round(this.scrollTop) === this.clientHeight;
-//   document.registration.accept.disabled = document.getElementById(
-//     'nextstep',
-//   ).disabled = !checkReading.read;
-//   checkReading.noticeBox.textContent = checkReading.read
-//     ? 'Thank you.'
-//     : 'Please, scroll and read the following text.';
-// }
 
-// onload = () => {
-//   const oToBeRead = document.getElementById('rules');
-//   checkReading.noticeBox = document.createElement('span');
-//   document.registration.accept.checked = false;
-//   checkReading.noticeBox.id = 'notice';
-//   oToBeRead.parentNode.insertBefore(checkReading.noticeBox, oToBeRead);
-//   oToBeRead.parentNode.insertBefore(document.createElement('br'), oToBeRead);
-//   oToBeRead.onscroll = checkReading;
-//   checkReading.call(oToBeRead);
-// };
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollHeight
+/*
+
+function checkReading() {
+  if (checkReading.read) {
+    return;
+  }
+  checkReading.read =
+    this.scrollHeight - Math.round(this.scrollTop) === this.clientHeight;
+  document.registration.accept.disabled = document.getElementById(
+    'nextstep',
+  ).disabled = !checkReading.read;
+  checkReading.noticeBox.textContent = checkReading.read
+    ? 'Thank you.'
+    : 'Please, scroll and read the following text.';
+}
+
+onload = () => {
+  const oToBeRead = document.getElementById('rules');
+  checkReading.noticeBox = document.createElement('span');
+  document.registration.accept.checked = false;
+  checkReading.noticeBox.id = 'notice';
+  oToBeRead.parentNode.insertBefore(checkReading.noticeBox, oToBeRead);
+  oToBeRead.parentNode.insertBefore(document.createElement('br'), oToBeRead);
+  oToBeRead.onscroll = checkReading;
+  checkReading.call(oToBeRead);
+};
+
+*/
