de99970f18826fd6143ab09b8a183ccbb9fb5cb9
Boula Akladyous <akladyous@gmail.com>
2023-05-20 15:49:00 -0400

    Untracked files on javascript

--- src/frontend/scrolling/scroll.js
+++ src/frontend/scrolling/scroll.js
@@ -2,14 +2,62 @@ const scroller = document.querySelector('.scroller');
 const scrollTop = document.querySelector('.scroll-top');
 const scrollHeight = document.querySelector('.scroll-height');
 const clientHeight = document.querySelector('.client-height');
+const scrollHeightMinusScrollTop = document.querySelector(
+  '.scrollHeightMinusScrollTop',
+);
+const read = document.querySelector('.read');
+const isScrolled = document.querySelector('.isScrolled');
 
 scroller.addEventListener('scroll', (event) => {
   scrollTop.textContent = scroller.scrollTop;
   scrollHeight.textContent = scroller.scrollHeight;
   clientHeight.textContent = scroller.clientHeight;
+  scrollHeightMinusScrollTop.textContent =
+    scroller.scrollHeight - Math.round(scroller.scrollTop);
 
-  const x = Math.abs(
-    scroller.scrollHeight - scroller.clientHeight - scroller.scrollTop,
-  );
-  console.log('x                     : ', scroller.scrollHeight);
+  read.textContent =
+    scroller.scrollHeight - Math.round(scroller.scrollTop) ===
+    scroller.clientHeight;
+
+  const rr = isTotallyScrolled(scroller);
+  console.log(rr);
+  isScrolled.textContent = rr.toString();
 });
+
+const isTotallyScrolled = (element) => {
+  return (
+    Math.abs(element.scrollHeight - element.clientHeight - element.scrollTop) <
+    1
+  );
+};
+
+onload = () => {
+  const read = document.querySelector('.read');
+  read.textContent =
+    scroller.scrollHeight - Math.round(scroller.scrollTop) ===
+    scroller.clientHeight;
+};
+// function checkReading() {
+//   if (checkReading.read) {
+//     return;
+//   }
+//   checkReading.read =
+//     this.scrollHeight - Math.round(this.scrollTop) === this.clientHeight;
+//   document.registration.accept.disabled = document.getElementById(
+//     'nextstep',
+//   ).disabled = !checkReading.read;
+//   checkReading.noticeBox.textContent = checkReading.read
+//     ? 'Thank you.'
+//     : 'Please, scroll and read the following text.';
+// }
+
+// onload = () => {
+//   const oToBeRead = document.getElementById('rules');
+//   checkReading.noticeBox = document.createElement('span');
+//   document.registration.accept.checked = false;
+//   checkReading.noticeBox.id = 'notice';
+//   oToBeRead.parentNode.insertBefore(checkReading.noticeBox, oToBeRead);
+//   oToBeRead.parentNode.insertBefore(document.createElement('br'), oToBeRead);
+//   oToBeRead.onscroll = checkReading;
+//   checkReading.call(oToBeRead);
+// };
