78491a33830b41f54eef395d5fbb1e9fee837166
Boula Akladyous <akladyous@gmail.com>
2023-05-20 10:33:43 -0400

    Untracked files on javascript

--- src/dom/basic/nodeInterface.js
+++ src/dom/basic/nodeInterface.js
@@ -1,6 +1,13 @@
-let node = document.querySelector('body').firstChild;
+let node = document.querySelector('.parent').firstChild;
 const nodes = [];
 
+const outputSection = document.querySelector('.output');
+const ul = document.createElement('ul');
+outputSection.appendChild(ul);
+
 while (node) {
-  node.node;
+  let listItem = document.createElement('li');
+  listItem.textContent = `Value of ${node.nodeName} : ${node.nodeValue}`;
+  outputSection.appendChild(listItem);
+  node = node.nextSibling;
 }
