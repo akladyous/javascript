054c427a277742f87f3c78c6e5c300eeaf42ced8
Boula Akladyous <akladyous@gmail.com>
2023-05-21 11:54:09 -0400

    Untracked files on javascript

--- src/Array/arrayMethods.js
+++ src/Array/arrayMethods.js
@@ -1,5 +1,9 @@
+// import { todos } from '../data/todos';
+
 let oneToFive = ['one', 'two', 'three', 'four', 'five'];
 let sixToTen = ['six', 'seven', 'eight', 'nine', 'ten'];
+const numWords = oneToFive.concat(sixToTen);
+
 let o1 = { one: 1, two: 2, three: 3, four: 4, five: 5 };
 let o2 = { six: 6, seven: 7, eight: 8, nine: 9, ten: 10 };
 
@@ -11,6 +15,8 @@ let nums = [...Array(5).keys()];
 const alphabets = Array.from([...Array(26).keys()], (n) =>
   String.fromCharCode(n + 65),
 );
+console.log('alphabets : ', alphabets);
+
 const splitArray = (array = [], cb) => {
   var midpoint = Math.ceil(array.length / 2);
   var firstHalf = array.slice(0, midpoint);
@@ -50,4 +56,29 @@ console.log(
 const rangeOfTen = [...Array(10).keys()];
 console.log('some : ', rangeOfTen.some(isEven));
 
-console.log('alphabets : ', alphabets);
+console.log('indexOf : ', numWords.indexOf('ten'));
+console.log('-'.repeat(50));
+
+console.log('Sparse Array');
+const sparse1 = [10, 20, , , , 60];
+console.log('[10, 20, , , , 60].length : ', sparse1.length);
+console.log(' ');
+sparse1.forEach((n) => console.log('[10, 20, , , , 60].forEach : ', n));
+console.log(' ');
+console.log('[...sparse1] spread operator', [...sparse1]);
+console.log(' ');
+console.log('Object.keys(sparse1) : ', Object.keys(sparse1));
+console.log(
+  'sparse1.filter(() => true) : ',
+  sparse1.filter(() => true),
+);
+console.log('sparse1.flat() : ', sparse1.flat());
+console.log(
+  '[10, 20, , , , 60].some((n) => !n) : ',
+  sparse1.some((n) => !n),
+  "-> It has no Falsy value, they just does't exists!!",
+);
+console.log(' ');
+console.log([1, 2, undefined, 4, undefined, 6].every(Boolean));
+console.log([1, 2, undefined, 4, undefined, 6].some((n) => !n));
+console.log([1, 2, null, 4, null, 6].some((n) => !n));
