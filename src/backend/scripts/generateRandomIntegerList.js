d563cd5612d990f751311b97ed9611c3ec0cd310
Boula Akladyous <akladyous@gmail.com>
2023-05-21 00:34:50 -0400

    Untracked files on javascript

---
+++ src/backend/scripts/generateRandomIntegerList.js
@@ -0,0 +1,11 @@
+const generateRandomIntegerList = (length, min, max) => {
+  const randomNumbers = new Array();
+
+  for (let i = 0; i < length; i++) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomNumbers.push(randomNumber);
+  }
+  return randomNumbers;
+};
+
+console.log(generateRandomIntegerList(10, 1, 5));
