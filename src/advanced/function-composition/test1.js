7378cdcea09f906c68ad987374aa5556ab79fcfa
Boula Akladyous <akladyous@gmail.com>
2023-05-24 13:13:32 -0400

    Untracked files on javascript

--- src/advanced/function-composition/test1.js
+++ src/advanced/function-composition/test1.js
@@ -4,6 +4,16 @@ const compose =
     functions.forEach((fn) => fn?.(...args));
 
 const add = (x) => (y) => x + y;
-const addX = add(1);
-const addY = addX(1);
-console.log(addY);
+const square = (x) => x * x;
+const double = (x) => x * 2;
+
+// Compose the functions
+const composedFn = compose(
+  add(5), // Add 5 to the argument
+  square, // Square the result
+  double, // Double the result
+);
+
+// Call the composed function
+const result = composedFn(3);
+console.log(result);
