d124db9a7f8a77be4277f334e44344ec8313eefa
Boula Akladyous <akladyous@gmail.com>
2023-06-02 16:00:56 -0400

    Untracked files on javascript

--- src/basic/basic.js
+++ src/basic/basic.js
@@ -1,3 +1,8 @@
 var str1 = 'hello';
 console.log(typeof str1);
 console.log(str1 instanceof String);
+console.log('-'.repeat(50));
+var int = 123;
+console.log('typeof int : ', typeof int);
+console.log('str1 instanceof Number : ', str1 instanceof Number);
+console.log('str1 instanceof Object : ', str1 instanceof Object);
---
+++ src/basic/built-in-objects.js
@@ -0,0 +1,15 @@
+/*
+Built-in Objects
+There are several other object subtypes, usually referred to as built-in objects. For some of them, their names seem to imply they are directly related to their simple primitive counterparts, but in fact, their relationship is more complicated, which we’ll explore shortly.
+
+String
+Number
+Boolean
+Object
+Function
+Array
+Date
+RegExp
+Error
+
+*/
