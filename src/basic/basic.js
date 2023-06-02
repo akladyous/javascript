59c34c28a15b62be1239b1b06aff01ae28ddaabd
Boula Akladyous <akladyous@gmail.com>
2023-06-02 16:04:12 -0400

    Untracked files on javascript

--- src/basic/basic.js
+++ src/basic/basic.js
@@ -1,8 +1,10 @@
-var str1 = 'hello';
-console.log(typeof str1);
-console.log(str1 instanceof String);
+var str = 'hello';
+console.log('typeof str : ', typeof str);
+console.log('str instanceof String : ', str instanceof String);
+console.log('str instanceof Object', str instanceof Object);
+
 console.log('-'.repeat(50));
 var int = 123;
 console.log('typeof int : ', typeof int);
-console.log('str1 instanceof Number : ', str1 instanceof Number);
-console.log('str1 instanceof Object : ', str1 instanceof Object);
+console.log('int instanceof Number : ', int instanceof Number);
+console.log('int instanceof Object : ', int instanceof Object);
