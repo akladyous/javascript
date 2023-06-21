86ecd58eae24e931b4d3d56b696ec49cf6970a6c
Boula Akladyous <akladyous@gmail.com>
2023-06-21 10:53:30 -0400

    Untracked files on javascript

--- src/basic/scopeChain/demo.js
+++ src/basic/scopeChain/demo.js
@@ -20,5 +20,18 @@ console.log(num1); // prints 10, num1 created in global scope
 console.log(num2); // Uncaught ReferenceError: num2 is not defined // ReferenceError: num2 is not defined
 console.log(num3); // Uncaught ReferenceError: num2 is not defined // ReferenceError: num2 is not defined
 // -----------------------------------------------------------------------
-
+function print() {
+  var num1 = 10;
+  let num2 = 20;
+  const num3 = 30;
+  console.log(num1); // function scope
+  console.log(num2); // function scope
+  console.log(num3); // function scope
+}
+console.log(print());
+console.log(num1); //  Reference error: num1 is not defined
+console.log(num2); // Reference error: num2 is not defined
+console.log(num3); // Reference Error: num3 is not defined
+// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // -----------------------------------------------------------------------
