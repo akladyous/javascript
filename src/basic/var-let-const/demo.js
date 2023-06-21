4f4cda18a6c9ba3173b1337c095288c0a1f1e234
Boula Akladyous <akladyous@gmail.com>
2023-06-21 11:11:54 -0400

    Untracked files on javascript

--- src/basic/var-let-const/demo.js
+++ src/basic/var-let-const/demo.js
@@ -1,6 +1,22 @@
-for (var i = 0; i < 5; i++) {
-  console.log('var i : ', i);
+for (var x = 0; x < 5; x++) {
+  console.log('var x : ', x);
 }
 console.log('outside the scope');
-console.log('value of i : ', i);
+console.log('value of x : ', x);
+// ---------------------------------------------
+console.log('-'.repeat(50));
+function test1() {
+  for (var i = 0; i < 5; i++) {
+    console.log('var i : ', i);
+  }
+  console.log('loop ends'); // llop ends
+  console.log('var i : ', i);
+}
+test1();
+console.log('var i : ', i); //Uncaught ReferenceError: i is not defined
+
+// ---------------------------------------------
+
+// ---------------------------------------------
+// ---------------------------------------------
 // ---------------------------------------------
