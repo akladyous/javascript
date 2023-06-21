51af9d0db45c2d799c4e5771295c052256ed6c48
Boula Akladyous <akladyous@gmail.com>
2023-06-21 10:59:54 -0400

    Untracked files on javascript

---
+++ src/basic/var-let-const/demo.js
@@ -0,0 +1,6 @@
+for (var i = 0; i < 5; i++) {
+  console.log('var i : ', i);
+}
+console.log('outside the scope');
+console.log('value of i : ', i);
+// ---------------------------------------------
