4f44e6677af1ed48964a59b47317954f52032062
Boula Akladyous <akladyous@gmail.com>
2023-06-20 22:22:50 -0400

    Untracked files on javascript

--- src/basic/objects/object.function.inheritance.js
+++ src/basic/objects/object.function.inheritance.js
@@ -1,26 +1,26 @@
-function People(first, last, gender){
-    this.first = first;
-    this.last = last;
-    this.gender = gender;
-    this.name = `${first} ${last}`
+function People(first, last, gender) {
+  this.first = first;
+  this.last = last;
+  this.gender = gender;
+  this.name = `${first} ${last}`;
 }
 
-function Teacher(first, last, gender, subject){
-    People.call(this, first, last, gender);
-    this.subject = subject;
+function Teacher(first, last, gender, subject) {
+  People.call(this, first, last, gender);
+  this.subject = subject;
 }
-Teacher.prototype.race = "white";
-Teacher.prototype.mansion = function(){
-    return this.subject.toUpperCase();
+Teacher.prototype.race = 'white';
+Teacher.prototype.mansion = function () {
+  return this.subject.toUpperCase();
 };
 
-var paolo = new People("boula", "akladyous", "male");
-var mary = new Teacher("Mary", "Smith", "Female", "math");
-mary.race = "black"
+var john = new People('John', 'Doe', 'male');
+var mary = new Teacher('Mary', 'Smith', 'Female', 'math');
+mary.race = 'black';
 
-console.log(paolo.name);
+console.log(john.name);
 console.log(mary.mansion());
 console.log(mary.race);
 
-// console.log(paolo.constructor);
+// console.log(john.constructor);
 console.log(mary.hasOwnProperty('race'));
