fa236cb99ae4ba99a1f696b798291c31346d5baa
Boula Akladyous <akladyous@gmail.com>
2023-06-29 00:04:48 -0400

    Untracked files on javascript

--- src/basic/objects/objectDemo.js
+++ src/basic/objects/objectDemo.js
@@ -16,9 +16,5 @@ console.log(Object.getOwnPropertyDescriptor(people, 'firstName'));
 console.log(Object.getOwnPropertyDescriptors(people));
 
 let l1 = [2, 4, 6, 8, 10];
-
-for (let x in l1) {
-  console.log(`Array l1 at index ${x} = ${l1.at(x)}`);
-}
 var it = l1[Symbol.iterator]();
 console.log('array iterator : ', it);
