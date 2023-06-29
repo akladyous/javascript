d4cef8f7fe1259237e8c0495dee2e7e79e47798a
Boula Akladyous <akladyous@gmail.com>
2023-06-29 08:42:38 -0400

    Untracked files on javascript

--- src/basic/objects/object.prototype.js
+++ src/basic/objects/object.prototype.js
@@ -1,22 +1,23 @@
-function foo(x){
-    this.x = x;
+function foo(x) {
+  this.x = x;
 }
 foo.prototype.y = 10;
-foo.prototype.calculate = function(z){
-    return this.x + this.y + z;
-}
+foo.prototype.sum = function (z) {
+  return this.x + this.y + z;
+};
 var t1 = new foo(10);
-console.log(t1.calculate(10));
+console.log(t1.sum(10));
 
-function baz(){
-    var x = 1;
-    return {
-        foo: function foo(){return ++x;},
-        bar: function bar(){return --x}
-    }
+function baz() {
+  var x = 1;
+  return {
+    foo: function foo() {
+      return ++x;
+    },
+    bar: function bar() {
+      return --x;
+    },
+  };
 }
 var clouser = new baz();
-console.log(
-    clouser.foo(),
-    clouser.bar()
-)
+console.log(clouser.foo(), clouser.bar());
